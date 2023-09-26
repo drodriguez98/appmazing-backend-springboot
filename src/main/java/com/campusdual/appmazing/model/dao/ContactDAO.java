7f25dc5d8393ef5eaf351317556f8d05a6c53936
Diego Rodriguez <diego25598@gmail.com>
2023-09-26 14:39:31 +0200

    Add Contact DAO & DTO

---
+++ src/main/java/com/campusdual/appmazing/model/dao/ContactDAO.java
@@ -0,0 +1,10 @@
+package com.campusdual.appmazing.model.dao;
+
+import com.campusdual.appmazing.model.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactDAO extends JpaRepository<Contact, Integer> {
+
+
+
+}
---
+++ src/main/java/com/campusdual/appmazing/model/dto/ContactDTO.java
@@ -0,0 +1,25 @@
+package com.campusdual.appmazing.model.dto;
+
+public class ContactDTO {
+
+    private int id;
+    private String name;
+    private String surname1;
+    private String surname2;
+    private String phone;
+    private String email;
+
+    public int getId() { return this.id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return this.name; }
+    public void setName(String name) { this.name = name; }
+    public String getSurname1() { return this.surname1; }
+    public void setSurname1(String surname) { this.surname1 = surname1; }
+    public String getSurname2() { return this.surname2; }
+    public void setSurname2(String surname) { this.surname2 = surname2; }
+    public String getPhone() { return this.phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+    public String getEmail() { return this.email; }
+    public void setEmail(String email) { this.email = email; }
+
+}
