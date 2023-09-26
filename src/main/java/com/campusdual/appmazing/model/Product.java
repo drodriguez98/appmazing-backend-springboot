fd5682340f4769ce7a8737f40eac0b9c363b3c70
Diego Rodriguez <diego25598@gmail.com>
2023-09-26 11:34:12 +0200

    Add entity Product

---
+++ src/main/java/com/campusdual/appmazing/model/Product.java
@@ -0,0 +1,47 @@
+// Modelo de entidades
+
+package com.campusdual.appmazing.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+// Entidad Products (elemento persistente que se puede almacenar y recuperar de la base de datos). Mete los datos en la tabla Products.
+
+@Entity
+
+@Table (name = "PRODUCTS")
+
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private int stock;
+    @Column
+    private BigDecimal price;
+
+    @Column
+    private boolean active;
+    @Column
+    private Date date_added;
+
+    // Getters y Setters
+
+    public int getId() { return this.id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return this.name; }
+    public boolean isActive() { return this.active; }
+    public BigDecimal getPrice() { return this.price; }
+    public int getStock() { return this.stock; }
+    public Date getDate_added() { return this.date_added; }
+    public void setName(String name) { this.name = name; }
+    public void setStock(int stock) { this.stock = stock; }
+    public void setPrice(BigDecimal price) { this.price = price; }
+    public void setActive(boolean active) { this.active = active; }
+    public void setDate_added(Date date_added) { this.date_added = date_added; }
+
+}
