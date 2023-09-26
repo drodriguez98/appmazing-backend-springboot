64a30b2dc020e45ccb2e409cd156988f4c5d9bd8
Diego Rodriguez <diego25598@gmail.com>
2023-09-26 14:41:04 +0200

    Add Product DAO & DTO

---
+++ src/main/java/com/campusdual/appmazing/model/dao/ProductDAO.java
@@ -0,0 +1,10 @@
+package com.campusdual.appmazing.model.dao;
+
+import com.campusdual.appmazing.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductDAO extends JpaRepository<Product, Integer> {
+
+
+
+}
---
+++ src/main/java/com/campusdual/appmazing/model/dto/ProductDTO.java
@@ -0,0 +1,30 @@
+// Modelo de entidades
+
+package com.campusdual.appmazing.model.dto;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class ProductDTO {
+
+    private int id;
+    private String name;
+    private int stock;
+    private BigDecimal price;
+    private boolean active;
+    private Date date_added;
+
+    public int getId() { return this.id; }
+    public String getName() { return this.name; }
+    public boolean isActive() { return this.active; }
+    public BigDecimal getPrice() { return this.price; }
+    public int getStock() { return this.stock; }
+    public Date getDate_added() { return this.date_added; }
+    public void setId(int id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setStock(int stock) { this.stock = stock; }
+    public void setPrice(BigDecimal price) { this.price = price; }
+    public void setActive(boolean active) { this.active = active; }
+    public void setDate_added(Date date_added) { this.date_added = date_added; }
+
+}
