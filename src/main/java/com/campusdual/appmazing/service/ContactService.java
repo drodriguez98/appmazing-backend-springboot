57b73697e9ca661057cb709ff7c4604356d7e24b
Diego Rodriguez <diego25598@gmail.com>
2023-09-27 14:15:07 +0200

    Add CRUD methods for Contacts and Products

---
+++ src/main/java/com/campusdual/appmazing/service/ContactService.java
@@ -0,0 +1,62 @@
+package com.campusdual.appmazing.service;
+
+import com.campusdual.appmazing.api.IContactService;
+import com.campusdual.appmazing.model.Contact;
+import com.campusdual.appmazing.model.dao.ContactDAO;
+import com.campusdual.appmazing.model.dto.ContactDTO;
+import com.campusdual.appmazing.model.dto.dtopmapper.ContactMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("ContactService")
+@Lazy
+public class ContactService implements IContactService {
+
+    @Autowired
+    private ContactDAO contactDAO;
+
+    @Override
+    public ContactDTO queryContact(ContactDTO contactDTO) {
+
+        Contact contact = ContactMapper.INSTANCE.toEntity(contactDTO);
+        return ContactMapper.INSTANCE.toDTO(this.contactDAO.getReferenceById(contact.getId()));
+
+    }
+
+    @Override
+    public List<ContactDTO> queryAllContacts() {
+
+        return ContactMapper.INSTANCE.toDTOList(contactDAO.findAll());
+
+    }
+
+    @Override
+    public int insertContact(ContactDTO contactDTO) {
+
+        Contact contact = ContactMapper.INSTANCE.toEntity(contactDTO);
+        this.contactDAO.saveAndFlush(contact);
+        return contact.getId();
+
+    }
+
+    @Override
+    public int updateContact(ContactDTO contactDTO) {
+
+        return this.insertContact(contactDTO);
+
+    }
+
+    @Override
+    public int deleteContact(ContactDTO contactDTO) {
+
+        int id = contactDTO.getId();
+        Contact contact = ContactMapper.INSTANCE.toEntity(contactDTO);
+        this.contactDAO.delete(contact);
+        return id;
+
+    }
+
+}
---
+++ src/main/java/com/campusdual/appmazing/service/ProductService.java
@@ -0,0 +1,62 @@
+package com.campusdual.appmazing.service;
+
+import com.campusdual.appmazing.api.IProductService;
+import com.campusdual.appmazing.model.Product;
+import com.campusdual.appmazing.model.dao.ProductDAO;
+import com.campusdual.appmazing.model.dto.ProductDTO;
+import com.campusdual.appmazing.model.dto.dtopmapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("ProductService")
+@Lazy
+public class ProductService implements IProductService {
+
+    @Autowired
+    private ProductDAO productDAO;
+
+    @Override
+    public ProductDTO queryProduct(ProductDTO productDTO) {
+
+        Product product = ProductMapper.INSTANCE.toEntity(productDTO);
+        return ProductMapper.INSTANCE.toDTO(this.productDAO.getReferenceById(product.getId()));
+
+    }
+
+    @Override
+    public List<ProductDTO> queryAllProducts() {
+
+        return ProductMapper.INSTANCE.toDTOList(productDAO.findAll());
+
+    }
+
+    @Override
+    public int insertProduct(ProductDTO productDTO) {
+
+        Product product = ProductMapper.INSTANCE.toEntity(productDTO);
+        this.productDAO.saveAndFlush(product);
+        return product.getId();
+
+    }
+
+    @Override
+    public int updateProduct(ProductDTO productDTO) {
+
+        return this.insertProduct(productDTO);
+
+    }
+
+    @Override
+    public int deleteProduct(ProductDTO productDTO) {
+
+        int id = productDTO.getId();
+        Product product = ProductMapper.INSTANCE.toEntity(productDTO);
+        this.productDAO.delete(product);
+        return id;
+
+    }
+
+}
