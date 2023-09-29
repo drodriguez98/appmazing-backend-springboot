30b04032f4fa732eac81d2b126e7280d038cdfcf
Diego Rodriguez <diego25598@gmail.com>
2023-09-29 15:05:49 +0200

    Add Product DTO (Updated)

--- src/main/java/com/campusdual/appmazing/model/dto/ProductDTO.java
+++ src/main/java/com/campusdual/appmazing/model/dto/ProductDTO.java
@@ -1,9 +1,9 @@
 package com.campusdual.appmazing.model.dto;
 
+import com.campusdual.appmazing.model.Category;
+
 import java.math.BigDecimal;
 import java.util.Date;
-
-// Igual que la clase Product pero sin notaciones.
 
 public class ProductDTO {
 
@@ -13,6 +13,7 @@ public class ProductDTO {
     private BigDecimal price;
     private boolean active;
     private Date date_added;
+    private CategoryDTO category_id;
 
     public int getId() { return this.id; }
     public String getName() { return this.name; }
@@ -20,11 +21,14 @@ public class ProductDTO {
     public BigDecimal getPrice() { return this.price; }
     public int getStock() { return this.stock; }
     public Date getDate_added() { return this.date_added; }
+    public CategoryDTO getCategory_id() { return category_id; }
+
     public void setId(int id) { this.id = id; }
     public void setName(String name) { this.name = name; }
     public void setStock(int stock) { this.stock = stock; }
     public void setPrice(BigDecimal price) { this.price = price; }
     public void setActive(boolean active) { this.active = active; }
     public void setDate_added(Date date_added) { this.date_added = date_added; }
+    public void setCategory_id(CategoryDTO category_id) { this.category_id = category_id; }
 
 }
