1f9c38c2b2345d8233a9914fecbdc61f56fd9148
Diego Rodriguez <diego25598@gmail.com>
2023-09-29 12:20:21 +0200

    Add Category DAO (Updated)

--- src/main/java/com/campusdual/appmazing/model/dao/CategoryDAO.java
+++ src/main/java/com/campusdual/appmazing/model/dao/CategoryDAO.java
@@ -3,4 +3,4 @@ package com.campusdual.appmazing.model.dao;
 import com.campusdual.appmazing.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryDAO extends JpaRepository<Category, Integer> { }
+public interface CategoryDAO extends JpaRepository<Category, Integer> {}
