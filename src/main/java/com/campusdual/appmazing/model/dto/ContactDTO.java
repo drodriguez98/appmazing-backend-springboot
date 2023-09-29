72eb5ee8c6c52011668adecae6b28b565b4fd9c4
Diego Rodriguez <diego25598@gmail.com>
2023-09-29 11:20:20 +0200

    Add Contact DTO (Updated)

--- src/main/java/com/campusdual/appmazing/model/dto/ContactDTO.java
+++ src/main/java/com/campusdual/appmazing/model/dto/ContactDTO.java
@@ -16,9 +16,9 @@ public class ContactDTO {
     public String getName() { return this.name; }
     public void setName(String name) { this.name = name; }
     public String getSurname1() { return this.surname1; }
-    public void setSurname1(String surname) { this.surname1 = surname1; }
+    public void setSurname1(String surname1) { this.surname1 = surname1; }
     public String getSurname2() { return this.surname2; }
-    public void setSurname2(String surname) { this.surname2 = surname2; }
+    public void setSurname2(String surname2) { this.surname2 = surname2; }
     public String getPhone() { return this.phone; }
     public void setPhone(String phone) { this.phone = phone; }
     public String getEmail() { return this.email; }
