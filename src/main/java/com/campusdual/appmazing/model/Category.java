4553a185efd65b5c87a393f0b7a3ce2899f6820c
Diego Rodriguez <diego25598@gmail.com>
2023-09-29 11:17:26 +0200

    Add Category entity

---
+++ src/main/java/com/campusdual/appmazing/model/Category.java
@@ -0,0 +1,23 @@
+package com.campusdual.appmazing.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CATEGORYS")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+
+    public int getId() { return this.id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return this.name; }
+
+    public void setName(String name) { this.name = name; }
+
+}
