7f4b468f4613700daca4832cd731d1ae090fe2e3
Diego Rodriguez <diego25598@gmail.com>
2023-09-29 11:18:19 +0200

    Add Category DAO & DTO

---
+++ src/main/java/com/campusdual/appmazing/model/dao/CategoryDAO.java
@@ -0,0 +1,6 @@
+package com.campusdual.appmazing.model.dao;
+
+import com.campusdual.appmazing.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryDAO extends JpaRepository<Category, Integer> { }
---
+++ src/main/java/com/campusdual/appmazing/model/dto/CategoryDTO.java
@@ -0,0 +1,16 @@
+package com.campusdual.appmazing.model.dto;
+
+public class CategoryDTO {
+
+    private int id;
+    private String name;
+
+    public int getId() { return this.id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return this.name; }
+
+    public void setName(String name) { this.name = name; }
+
+}
