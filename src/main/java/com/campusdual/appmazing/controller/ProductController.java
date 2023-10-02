e96ea6c1ab34f282e6cf087c2872adc4f05facbe
Diego Rodriguez <diego25598@gmail.com>
2023-10-02 18:28:20 +0200

    Add Product Controller (Updated)

--- src/main/java/com/campusdual/appmazing/controller/ProductController.java
+++ src/main/java/com/campusdual/appmazing/controller/ProductController.java
@@ -4,7 +4,10 @@ import com.campusdual.appmazing.api.IProductService;
 import com.campusdual.appmazing.model.dto.ProductDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/products")
@@ -17,27 +20,36 @@ public class ProductController {
     public String testController() { return "Product controller works!"; }
 
     @PostMapping
-    public String testController(@RequestBody String name) { return "Product controller works!, " + name; }
+    public String testController (@RequestBody String name) { return "Product controller works!, " + name; }
 
     @GetMapping(value = "/testMethod")
     public String testControllerMethod() { return "Product controller method works!"; }
 
     @PostMapping(value = "/get")
-    public ProductDTO queryProduct(@RequestBody ProductDTO product) { return this.productService.queryProduct(product); }
+    public ProductDTO queryProduct (@RequestBody ProductDTO product) { return this.productService.queryProduct(product); }
 
     @GetMapping(value = "/getAll")
     public List<ProductDTO> queryAllProducts() { return this.productService.queryAllProducts(); }
 
     @PostMapping(value = "/add")
-    public int insertProduct(@RequestBody ProductDTO product) { return this.productService.insertProduct(product); }
+    public int insertProduct (@RequestBody ProductDTO product) { return this.productService.insertProduct(product); }
 
     @PutMapping(value = "/update")
-    public int updateProduct(@RequestBody ProductDTO product) { return this.productService.updateProduct(product); }
+    public int updateProduct (@RequestBody ProductDTO product) { return this.productService.updateProduct(product); }
 
     @PostMapping(value = "/delete")
-    public int deleteProduct(@RequestBody ProductDTO product) { return this.productService.deleteProduct(product); }
+    public int deleteProduct (@RequestBody ProductDTO product) { return this.productService.deleteProduct(product); }
 
     @PutMapping(value = "/buy")
-    public int buyProduct(@RequestBody ProductDTO productDTO) { return this.productService.buyProduct(productDTO, 5); }
+    public int buyProduct (@RequestBody ProductDTO productDTO) { return this.productService.buyProduct(productDTO, 5); }
+
+    @PostMapping(value = "/buyAndShowPrice")
+    public BigDecimal buyAndShow (@RequestBody ProductDTO productDTO) {
+
+        int quantity = 5;
+        this.productService.buyProduct(productDTO, quantity);
+        return this.productService.calculateTotalPrice(productDTO, quantity);
+
+    }
 
 }
